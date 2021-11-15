8167626d2c1197b9d97dd6f543aa474b5ca09e67
Natham Fernandes <natham.fr@gmail.com>
2021-11-15 17:36:03 -0300

    Arquivo API Cotação Finalizado

--- arquivo-api.py
+++ arquivo-api.py
@@ -1,1 +1,24 @@
-arquivo-api
+import pip._vendor.requests
+import json
+
+while True:
+    identi = input('Insira o código da moeda que você pretende converter (USD, EUR, etc.): ').upper()
+    valreal = float(input('Insira a quantidade de dinheiro você possui, em R$: '))
+
+
+    cotacoes = pip._vendor.requests.get("https://economia.awesomeapi.com.br/json/all")
+    cotacoes = cotacoes.json()
+    cotacao = float(cotacoes[identi]['bid'])
+
+    print(' ')
+    print('Cotação do {}: {:.2f}'.format(identi, cotacao))
+
+    valabso = valreal/cotacao
+
+    print('Convertendo R${:.2f} em {}, o valor total convertido fica: {} {:.3f}.'.format(valreal, identi, identi, valabso))
+    print(' ')
+
+    x = int(input('Gostaria de rodar o programa novamente? \n - Se sim, digite "1". \n - Se não, digite "2". \n'))
+    if x == 2:
+        break
+
